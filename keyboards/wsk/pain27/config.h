ece90e02a790cf9ed46ba6e895ee29fec7beac9d
gnm3 <gnm3@gatech.edu>
2021-05-28 15:39:53 -0400

    Add  #define IGNORE_MOD_TAP_INTERRUPT

--- keyboards/wsk/pain27/config.h
+++ keyboards/wsk/pain27/config.h
@@ -29,6 +29,7 @@
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
+#define IGNORE_MOD_TAP_INTERRUPT
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
