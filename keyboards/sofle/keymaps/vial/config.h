fe3f1d76c501394dfd31998d4cbb1a9714c75b59
GerardoNevarez <GerardoNevarez@users.noreply.github.com>
2022-05-11 23:50:53 -0400

    Sofle with RGB Matrix

--- keyboards/sofle/keymaps/vial/config.h
+++ keyboards/sofle/keymaps/vial/config.h
@@ -28,3 +28,59 @@
 #define VIAL_UNLOCK_COMBO_COLS { 0, 0, 0, 0 }
 
 #define ENCODER_RESOLUTIONS { 4, 2 }
+
+#define RGB_DI_PIN D3
+
+#ifdef RGB_MATRIX_ENABLE
+
+#define RGBLED_NUM 58
+#define DRIVER_LED_TOTAL RGBLED_NUM
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+#define RGB_MATRIX_HUE_STEP 8
+#define RGB_MATRIX_SAT_STEP 8
+#define RGB_MATRIX_VAL_STEP 8
+#define RGB_MATRIX_SPD_STEP 10
+
+
+#define RGB_MATRIX_SPLIT {29,29}
+#define SPLIT_TRANSPORT_MIRROR
+#define VIALRGB_NO_DIRECT
+
+
+#define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+#define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_RAINDROPS
+
+
+//#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+//#define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+
+
+//define RGB_MATRIX_KEYPRESSES
+//#define ENABLE_RGB_MATRIX_SPLASH
+//#define ENABLE_RGB_MATRIX_MULTISPLASH
+//#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+#define RGBLIGHT_SPLIT
+#define RGBLED_NUM 58
+#define RGB_SPLIT {29,29}
+#define RGBLIGHT_LIMIT_VAL 120
+//   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+// /*== all animations enable ==*/
+//   #define RGBLIGHT_ANIMATIONS
+// /*== or choose animations ==*/
+//   #define RGBLIGHT_EFFECT_BREATHING
+   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//   #define RGBLIGHT_EFFECT_SNAKE
+//   #define RGBLIGHT_EFFECT_KNIGHT
+ #define RGBLIGHT_EFFECT_CHRISTMAS
+   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+   #define RGBLIGHT_EFFECT_RGB_TEST
+//   #define RGBLIGHT_EFFECT_ALTERNATING
+#endif
+
