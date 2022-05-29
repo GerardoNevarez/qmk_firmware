b99da371c17f6f588e1d7bd8de1ae6919c9848a2
gnm3 <gnm3@gatech.edu>
2022-05-29 01:20:52 -0400

    Corne with OLED, RGB underglow

--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -1,5 +1,5 @@
-MOUSEKEY_ENABLE     = yes     # Mouse keys
-RGBLIGHT_ENABLE     = no      # Enable WS2812 RGB underlight.
+MOUSEKEY_ENABLE     = no     # Mouse keys
+RGBLIGHT_ENABLE     = yes      # Enable WS2812 RGB underlight.
 VIA_ENABLE          = yes     # Enable VIA
 VIAL_ENABLE         = yes     # Enable VIAL
 OLED_ENABLE         = yes
@@ -8,6 +8,8 @@ EXTRAKEY_ENABLE     = yes
 LTO_ENABLE          = yes
 QMK_SETTINGS        = no
 COMBO_ENABLE        = no
-BOOTMAGIC_ENABLE    = yes
+TAP_DANCE_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+BOOTMAGIC_ENABLE    = no
 MAGIC_ENABLE        = no
 
