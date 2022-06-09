dfd72df56109130927a6cfea8b6cf49cc89e9e93
GerardoNevarez <GerardoNevarez@users.noreply.github.com>
2022-06-08 22:59:01 -0400

    wip

--- keyboards/sofle/rev1/config.h
+++ keyboards/sofle/rev1/config.h
@@ -68,9 +68,49 @@ B6  - 10
 #define SERIAL_USE_MULTI_TRANSACTION
 //#define SOFT_SERIAL_PIN D2
 #define SOFT_SERIAL_PIN 1
-
+    
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 
 /* Use 1000hz polling */
 #define USB_POLLING_INTERVAL_MS 1
+
+///////////////////////////
+/*
+
+// Soft Serial Transaction Descriptor
+typedef struct _SSTD_t  {
+    uint8_t *status;
+    uint8_t initiator2target_buffer_size;
+    uint8_t *initiator2target_buffer;
+    uint8_t target2initiator_buffer_size;
+    uint8_t *target2initiator_buffer;
+} SSTD_t;
+#define TID_LIMIT( table ) (sizeof(table) / sizeof(SSTD_t))
+
+ 
+// initiator is transaction start side
+void soft_serial_initiator_init(SSTD_t *sstd_table, int sstd_table_size);
+// target is interrupt accept side
+void soft_serial_target_init(SSTD_t *sstd_table, int sstd_table_size);
+*/ 
+// initiator resullt
+#define TRANSACTION_END 0
+#define TRANSACTION_NO_RESPONSE 0x1
+#define TRANSACTION_DATA_ERROR  0x2
+#define TRANSACTION_TYPE_ERROR  0x4
+
+// target status
+// *SSTD_t.status has
+//   initiator:
+//       TRANSACTION_END
+//    or TRANSACTION_NO_RESPONSE
+//    or TRANSACTION_DATA_ERROR
+//   target:
+//       TRANSACTION_DATA_ERROR
+//    or TRANSACTION_ACCEPTED
+#define TRANSACTION_ACCEPTED 0x8
+
+//#ifdef SERIAL_USE_MULTI_TRANSACTION
+//int  soft_serial_get_and_clean_status(int sstd_index);
+//#endif
